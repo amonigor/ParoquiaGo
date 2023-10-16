df52e5c7cae0e9f1f3516f402a5c4feef5d8f178
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-10-16 20:27:46 -0300

    fix env import

--- android/app/src/main/java/com/paroquiago/MainApplication.java
+++ android/app/src/main/java/com/paroquiago/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.lugg.RNCConfig.RNCConfigPackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
