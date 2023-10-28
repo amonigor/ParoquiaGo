6f204beb937b37d6f58343e11ddb0c09d10e1945
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-10-28 13:48:24 -0300

    only shows the buttons when userLocation is setted

--- src/components/SearchArea/SearchAreaContainer.tsx
+++ src/components/SearchArea/SearchAreaContainer.tsx
@@ -3,19 +3,27 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import { GpsStatusComponent } from './GpsStatus/GpsStatusComponent';
 import { styles } from './SearchAreaStyle';
 import { GpsRollbackComponent } from './GpsRollback/GpsRollbackComponent';
+import { useAtomValue } from 'jotai';
+import { userLocationAtom } from '../../atoms/map';
 
 export const SearchAreaContainer = () => {
+  const userLocation = useAtomValue(userLocationAtom);
+
   return (
     <View style={styles.container}>
       <View style={styles.rightItems}>
         <GpsStatusComponent />
-        <GpsRollbackComponent />
+        {!!userLocation ? <GpsRollbackComponent /> : <></>}
       </View>
-      <TouchableOpacity activeOpacity={0.75}>
-        <View style={{ ...styles.button, ...styles.withShadows }}>
-          <Text style={styles.buttonText}>Buscar mais próximo</Text>
-        </View>
-      </TouchableOpacity>
+      {!!userLocation ? (
+        <TouchableOpacity activeOpacity={0.75}>
+          <View style={{ ...styles.button, ...styles.withShadows }}>
+            <Text style={styles.buttonText}>Buscar mais próximo</Text>
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <></>
+      )}
       <View style={{ ...styles.searchArea, ...styles.withShadows }}>
         <Text>Placeholder text</Text>
       </View>
